4f6d5577da90fe4afd761a7a699f596ec72fd2a6
Felipe Castilhos <felipecastilhos@gmail.com>
2021-11-16 00:31:08 -0300

    build: create Dependencies objects

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,46 +1,51 @@
-object Versions {
+object DependencyVersions {
     const val androidxCore = "1.7.0"
+    const val androidxAppCompat = "1.3.1"
+
     const val composeVersion = "1.0.5"
     const val androidxLifecycle = "2.4.0"
-    const val androidxAppCompat = "1.3.1"
     const val androidMaterial = "1.4.0"
     const val activityCompose = "1.4.0"
     const val jUnitVersion = "4.13.2"
     const val androidxJunit = "1.1.3"
     const val androidxEspresso = "3.4.0"
     const val androidxComposeJunit = "1.0.5"
+    const val kotlinCompilerExtension = "1.0.1"
 }
 
-object Dependencies {
+sealed class Dependencies {
     object Core {
-        const val androidxCore = "androidx.core:core-ktx:${Versions.androidxCore}"
+        const val androidxCore = "androidx.core:core-ktx:${DependencyVersions.androidxCore}"
     }
 
     object Compose {
-        const val ui = "androidx.compose.ui:ui:${Versions.composeVersion}"
-        const val material = "androidx.compose.material:material:${Versions.composeVersion}"
-        const val uiTooling = "androidx.compose.ui:ui-tooling-preview:${Versions.composeVersion}"
+        const val ui = "androidx.compose.ui:ui:${DependencyVersions.composeVersion}"
+        const val material = "androidx.compose.material:material:${DependencyVersions.composeVersion}"
+        const val uiToolingPreview = "androidx.compose.ui:ui-tooling-preview:${DependencyVersions.composeVersion}"
+        const val composeUiTooling = "androidx.compose.ui:ui-tooling:${DependencyVersions.composeVersion}"
     }
 
     object AndroidLifecycle {
-        const val viewModelCompose = "androidx.lifecycle:lifecycle-viewmodel-compose:${Versions.androidxLifecycle}"
-        const val runtimeKtx = "androidx.lifecycle:lifecycle-runtime-ktx:${Versions.androidxLifecycle}"
+        const val viewModelCompose = "androidx.lifecycle:lifecycle-viewmodel-compose:${DependencyVersions.androidxLifecycle}"
+        const val runtimeKtx = "androidx.lifecycle:lifecycle-runtime-ktx:${DependencyVersions.androidxLifecycle}"
     }
 
     object Ui {
-        const val appCompat = "androidx.appcompat:appcompat:${Versions.androidxAppCompat}"
-        const val material = "com.google.android.material:material:${Versions.androidMaterial}"
-        const val activityCompose = "androidx.activity:activity-compose:${Versions.activityCompose}"
+        const val appCompat = "androidx.appcompat:appcompat:${DependencyVersions.androidxAppCompat}"
+        const val material = "com.google.android.material:material:${DependencyVersions.androidMaterial}"
+        const val activityCompose = "androidx.activity:activity-compose:${DependencyVersions.activityCompose}"
     }
 
-    object UnitTests {
-        const val jUnit = "junit:junit:${Versions.jUnitVersion}"
-        const val androidxExtjUnit = "androidx.test.ext:junit:${Versions.androidxJunit}"
-        const val espresso = "androidx.test.espresso:espresso-core:${Versions.androidxEspresso}"
-        const val composejUnit = "androidx.compose.ui:ui-test-junit4:${Versions.androidxComposeJunit}}"
+    object Tests {
+        const val jUnit = "junit:junit:${DependencyVersions.jUnitVersion}"
+        const val androidxExtjUnit = "androidx.test.ext:junit:${DependencyVersions.androidxJunit}"
+        const val espresso = "androidx.test.espresso:espresso-core:${DependencyVersions.androidxEspresso}"
+        const val composejUnit = "androidx.compose.ui:ui-test-junit4:${DependencyVersions.androidxComposeJunit}"
     }
+}
 
-    object DebugTooling {
-        const val composeUiTooling = "androidx.compose.ui:ui-test-junit4:1.0.5"
-    }
+object DependencyPlugins {
+    const val androidApplication = "com.android.application"
+    const val kotlinAndroid = "kotlin-android"
+    const val kotlinAndroidExtensions = "kotlin-android-extensions"
 }
