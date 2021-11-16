d86369ca51bc28d6a025d1d933750fbb3fe0b8c8
Felipe Castilhos <felipecastilhos@gmail.com>
2021-11-15 21:18:32 -0300

    wip: break tests

--- app/src/androidTest/java/com/github/felipecastilhos/pokedexandroid/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/github/felipecastilhos/pokedexandroid/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.github.felipecastilhos.pokedexandroid", appContext.packageName)
+        assertEquals("com.github.felipecastilhos.pokedexandroidddd", appContext.packageName)
     }
 }
