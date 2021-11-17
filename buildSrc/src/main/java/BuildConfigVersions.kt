50751fec65ec834db6a94697693d38e08d31e2d2
Felipe Castilhos <felipecastilhos@gmail.com>
2021-11-16 22:22:57 -0300

    wip :: breaks ktlint

--- buildSrc/src/main/java/BuildConfigVersions.kt
+++ buildSrc/src/main/java/BuildConfigVersions.kt
@@ -2,6 +2,11 @@ object BuildConfigVersions {
     const val compileSdk = 31
     const val minSdk = 23
     const val targetSdk = 31
+
+
+
+
+    
     const val versionCode = 1
     const val versionName = "0.0.2"
 }
