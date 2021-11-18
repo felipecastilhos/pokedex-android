781b336ef831ee9f4364e5beaaf8d7c2089278ec
Felipe Castilhos <felipecastilhos@gmail.com>
2021-11-18 01:10:06 -0300

    feat: init LogHandler in PokedexApplication

--- app/src/main/java/com/github/felipecastilhos/pokedexandroid/PokedexApplication.kt
+++ app/src/main/java/com/github/felipecastilhos/pokedexandroid/PokedexApplication.kt
@@ -1,7 +1,20 @@
 package com.github.felipecastilhos.pokedexandroid
 
 import android.app.Application
+import com.github.felipecastilhos.pokedexandroid.logs.LogHandler
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class PokedexApplication : Application()
+class PokedexApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        dependenciesInitialize()
+    }
+
+    /**
+     * Inits all projects dependencies.
+     */
+    private fun dependenciesInitialize() {
+        LogHandler.init()
+    }
+}
