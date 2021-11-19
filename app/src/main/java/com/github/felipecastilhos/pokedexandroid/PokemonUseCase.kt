f04431e598998cb6f85783165010cf9f10ea5c27
Felipe Castilhos <felipecastilhos@gmail.com>
2021-11-18 21:33:20 -0300

    nit: remove empty constructor

--- app/src/main/java/com/github/felipecastilhos/pokedexandroid/PokemonUseCase.kt
+++ app/src/main/java/com/github/felipecastilhos/pokedexandroid/PokemonUseCase.kt
@@ -2,7 +2,7 @@ package com.github.felipecastilhos.pokedexandroid
 
 import com.apollographql.apollo.coroutines.await
 
-class PokemonUseCase() {
+class PokemonUseCase {
     suspend fun search(): GetPokemonQuery.Data? {
         val apolloClient = PokedexApolloClient.build()
         return apolloClient?.query(GetPokemonQuery())?.await()?.data
