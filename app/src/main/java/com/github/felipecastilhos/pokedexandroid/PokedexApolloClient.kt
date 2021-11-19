3f41d90f6f018a4aae8bb3e4a6f2ac51549f9a47
Felipe Castilhos <felipecastilhos@gmail.com>
2021-11-18 21:33:43 -0300

    nit: convert PokedexApolloClient to an object

--- app/src/main/java/com/github/felipecastilhos/pokedexandroid/PokedexApolloClient.kt
+++ app/src/main/java/com/github/felipecastilhos/pokedexandroid/PokedexApolloClient.kt
@@ -5,25 +5,23 @@ import com.github.felipecastilhos.pokedexandroid.logs.LogHandler
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 
-class PokedexApolloClient {
-    companion object {
-        fun build(): ApolloClient? = ApolloClient.builder()
-            .serverUrl(Enviroment.Production.graphQlUrl)
-            .okHttpClient(buildOkHttpClient())
-            .build()
-    }
-}
-
-private fun buildOkHttpClient(): OkHttpClient =
-    OkHttpClient.Builder()
-        .addInterceptor(buildLogInterceptor(HttpLoggingInterceptor.Level.BODY))
+object PokedexApolloClient {
+    fun build(): ApolloClient? = ApolloClient.builder()
+        .serverUrl(Enviroment.Production.graphQlUrl)
+        .okHttpClient(buildOkHttpClient())
         .build()
 
-fun buildLogInterceptor(level: HttpLoggingInterceptor.Level): HttpLoggingInterceptor {
-    val logger = HttpLoggingInterceptor.Logger { message -> LogHandler.d(message) }
-    val logging = HttpLoggingInterceptor(logger)
-    logging.level = level
-    return logging
+    private fun buildOkHttpClient(): OkHttpClient =
+        OkHttpClient.Builder()
+            .addInterceptor(buildLogInterceptor(HttpLoggingInterceptor.Level.BODY))
+            .build()
+
+    fun buildLogInterceptor(level: HttpLoggingInterceptor.Level): HttpLoggingInterceptor {
+        val logger = HttpLoggingInterceptor.Logger { message -> LogHandler.d(message) }
+        val logging = HttpLoggingInterceptor(logger)
+        logging.level = level
+        return logging
+    }
 }
 
 sealed class Enviroment(val graphQlUrl: String) {
