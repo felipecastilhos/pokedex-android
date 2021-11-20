57bf92303b6284d69d0b3c38680c819f9c33cbea
Felipe Castilhos <felipecastilhos@gmail.com>
2021-11-19 23:12:51 -0300

    feat: add dispatcher providers

---
+++ app/src/main/java/com/github/felipecastilhos/pokedexandroid/coroutines/DispatcherProvider.kt
@@ -0,0 +1,34 @@
+package com.github.felipecastilhos.pokedexandroid.coroutines
+
+import kotlinx.coroutines.CoroutineDispatcher
+
+/**
+ * Class to abstract the direct usage of dispatcher
+ * This helps to use inject coroutines in tests
+ */
+interface DispatcherProvider {
+    /**
+     +-----------------------------------+
+     | Main thread on Android, interact  |
+     | with the UI and perform light     |
+     | work                              |
+     +-----------------------------------+
+     | - Calling suspend functions       |
+     | - Call UI functions               |
+     | - Updating LiveData               |
+     +-----------------------------------+
+     */
+    val main: CoroutineDispatcher
+
+    /**
+     +-----------------------------------+
+     | Optimized for disk and network IO |
+     | off the main thread               |
+     +-----------------------------------+
+     | - Database                        |
+     | - Reading/writing files           |
+     | - Networking                      |
+     +-----------------------------------+
+     */
+    val io: CoroutineDispatcher
+}
