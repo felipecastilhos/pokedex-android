93b3ac3ce8a12c5fb6186379bcdb7012d56c2cae
Felipe Castilhos <felipecastilhos@gmail.com>
2021-11-21 18:04:03 -0300

    feat: presenting fetch data

--- app/src/main/java/com/github/felipecastilhos/pokedexandroid/features/home/HomeActivity.kt
+++ app/src/main/java/com/github/felipecastilhos/pokedexandroid/features/home/HomeActivity.kt
@@ -4,14 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.tooling.preview.Preview
-import com.github.felipecastilhos.pokedexandroid.GetPokemonQuery
 import com.github.felipecastilhos.pokedexandroid.core.datasource.Resource
 import com.github.felipecastilhos.pokedexandroid.core.ui.theme.PokedexandroidTheme
 import com.github.felipecastilhos.pokedexandroid.features.home.domain.models.Pokemon
@@ -38,10 +37,7 @@ class HomeActivity : ComponentActivity() {
                             Text("Ops, algo deu errado :(")
                         }
                         is Resource.Success -> {
-                            SearchPokemonResult(
-                                pokemonName = "Dragonite",
-                                pokemonNumber = (e as Resource.Success<Pokemon?>).data?.pokedexNumber
-                            )
+                            (e as Resource.Success<Pokemon?>).data?.let { SearchPokemonResult(it) }
                         }
                     }
                 }
@@ -52,21 +48,13 @@ class HomeActivity : ComponentActivity() {
 
 @Composable
 fun SearchPokemonResult(
-    pokemonName: String,
-    pokemonNumber: Int?
+    pokemon: Pokemon
 ) {
-    Text(text = "This is a $pokemonName. Pokemon Nbr.: ${pokemonNumber ?: "not found"}")
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    PokedexandroidTheme {
-        Greeting("Android")
+    Column {
+        Text("Pokemon Specie: ${pokemon.species}.")
+        Text("Number: ${pokemon.pokedexNumber}")
+        Text("Height: ${pokemon.height} ft")
+        Text("Weight: ${pokemon.weight} lbs")
+        Text("Info: ${pokemon.flavorText.first().flavor}")
     }
 }
--- app/src/main/java/com/github/felipecastilhos/pokedexandroid/features/home/domain/repository/DomainModelMappers.kt
+++ app/src/main/java/com/github/felipecastilhos/pokedexandroid/features/home/domain/repository/DomainModelMappers.kt
@@ -6,6 +6,9 @@ import com.github.felipecastilhos.pokedexandroid.features.home.domain.models.Fla
 import com.github.felipecastilhos.pokedexandroid.features.home.domain.models.Pokemon
 import com.github.felipecastilhos.pokedexandroid.features.home.domain.models.PokemonTypes
 
+/**
+ * Maps [GetPokemonQuery.GetPokemon] to [Pokemon] domain model
+ */
 fun GetPokemonQuery.GetPokemon.mapToDomainModel(): Pokemon = Pokemon(
     pokedexNumber = this.num,
     species = this.species,
@@ -15,17 +18,29 @@ fun GetPokemonQuery.GetPokemon.mapToDomainModel(): Pokemon = Pokemon(
     flavorText = this.flavorTexts.toFlavorListDomainModel()
 )
 
+/**
+ * Maps [GetPokemonQuery.FlavorText] list to [FlavorText] domain model list
+ */
 fun List<GetPokemonQuery.FlavorText>.toFlavorListDomainModel(): List<FlavorText> = map {
     it.toFlavorListDomainModel()
 }
 
+/**
+ * Maps [GetPokemonQuery.FlavorText] to [FlavorText] domain model
+ */
 fun GetPokemonQuery.FlavorText.toFlavorListDomainModel(): FlavorText =
     FlavorText(game = game, flavor = flavor)
 
+/**
+ * Maps List<String> to list of [PokemonTypes] domain model
+ */
 fun List<String>.toTypesDomainModel(): List<PokemonTypes?> = map {
     it.mapToTypesDomainModel()
 }
 
+/**
+ * Maps List<String> to [PokemonTypes] domain model
+ */
 fun String.mapToTypesDomainModel(): PokemonTypes? =
     try {
         PokemonTypes.valueOf(this)
