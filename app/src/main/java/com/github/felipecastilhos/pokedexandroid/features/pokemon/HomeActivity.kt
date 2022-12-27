14f731124c443202c429227b26a70a6587569c4b
Felipe Castilhos <felipecastilhos@gmail.com>
2022-12-27 10:14:59 -0300

    feat: openshowkaze

--- app/src/main/java/com/github/felipecastilhos/pokedexandroid/features/pokemon/HomeActivity.kt
+++ app/src/main/java/com/github/felipecastilhos/pokedexandroid/features/pokemon/HomeActivity.kt
@@ -8,10 +8,12 @@ import androidx.compose.material.Button
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import com.airbnb.android.showkase.models.Showkase
 import com.github.felipecastilhos.pokedexandroid.PokemonNavGraph
 import com.github.felipecastilhos.pokedexandroid.core.ui.theme.ColorScheme
 import com.github.felipecastilhos.pokedexandroid.core.ui.theme.PokedexAndroidTheme
 import com.github.felipecastilhos.pokedexandroid.core.ui.theme.PokedexTheme
+import com.github.felipecastilhos.pokedexandroid.getBrowserIntent
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -19,13 +21,11 @@ class HomeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            var isDarkTheme by remember { mutableStateOf(false) }
-            val schema = changeColorSchema(isDarkTheme = isDarkTheme)
-                PokedexAndroidTheme(darkTheme = isDarkTheme, colors = schema) {
-                    PokemonNavGraph()
-                    Button(onClick = { isDarkTheme = !isDarkTheme }) {
-                        Text("Change Schema")
-                    }
+            PokedexAndroidTheme {
+                PokemonNavGraph()
+                Button(onClick = { startActivity(Showkase.getBrowserIntent(this)) }) {
+                    Text("Change Schema")
+                }
             }
         }
     }
