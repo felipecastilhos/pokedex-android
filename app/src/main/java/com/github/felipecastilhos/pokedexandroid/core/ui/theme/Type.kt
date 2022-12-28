7b96a8a37f6816d31d0d46674b90076a21a78efd
Felipe Castilhos <felipecastilhos@gmail.com>
2022-12-28 09:52:16 -0300

    feat: add typography

--- app/src/main/java/com/github/felipecastilhos/pokedexandroid/core/ui/theme/Type.kt
+++ app/src/main/java/com/github/felipecastilhos/pokedexandroid/core/ui/theme/Type.kt
@@ -2,27 +2,117 @@ package com.github.felipecastilhos.pokedexandroid.core.ui.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.sp
+import com.github.felipecastilhos.pokedexandroid.R
 
 // Set of Material typography styles to start with
-val Typography = Typography(
-    body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
-    ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
-    )
-    */
+val MaterialTypography = Typography(
+    h1 = PokedexTypographyScheme.bold.h1,
+    h3 = PokedexTypographyScheme.bold.h2,
+    body1 = PokedexTypographyScheme.regular.body1
 )
+
+sealed class PokedexTypographyScheme(
+    val regular: PokedexTypography,
+    val bold: PokedexTypography
+) {
+    companion object :
+        PokedexTypographyScheme(regular = PokedexTypography.regular, bold = PokedexTypography.bold)
+}
+
+class PokedexTypography(
+    val h1: TextStyle,
+    val h2: TextStyle,
+    val body1: TextStyle,
+    val body2: TextStyle,
+    val subtitle: TextStyle
+) {
+    companion object {
+        val regular = PokedexTypography(
+            h1 = TextStyle(
+                fontFamily = PokedexFontFamily.poppins,
+                fontWeight = FontWeight.Normal,
+                fontSize = TypographySize.s500
+            ),
+            h2 = TextStyle(
+                fontFamily = PokedexFontFamily.poppins,
+                fontWeight = FontWeight.Normal,
+                fontSize = TypographySize.s400
+            ),
+            body1 = TextStyle(
+                fontFamily = PokedexFontFamily.poppins,
+                fontWeight = FontWeight.Normal,
+                fontSize = TypographySize.s300
+            ),
+            body2 = TextStyle(
+                fontFamily = PokedexFontFamily.poppins,
+                fontWeight = FontWeight.Normal,
+                fontSize = TypographySize.s200
+            ),
+            subtitle = TextStyle(
+                fontFamily = PokedexFontFamily.poppins,
+                fontWeight = FontWeight.Normal,
+                fontSize = TypographySize.s100
+            ),
+        )
+
+        val bold = PokedexTypography(
+            h1 = TextStyle(
+                fontFamily = PokedexFontFamily.poppins,
+                fontWeight = FontWeight.Bold,
+                fontSize = TypographySize.s500
+            ),
+            h2 = TextStyle(
+                fontFamily = PokedexFontFamily.poppins,
+                fontWeight = FontWeight.Bold,
+                fontSize = TypographySize.s400
+            ),
+            body1 = TextStyle(
+                fontFamily = PokedexFontFamily.poppins,
+                fontWeight = FontWeight.Bold,
+                fontSize = TypographySize.s300
+            ),
+            body2 = TextStyle(
+                fontFamily = PokedexFontFamily.poppins,
+                fontWeight = FontWeight.Bold,
+                fontSize = TypographySize.s200
+            ),
+            subtitle = TextStyle(
+                fontFamily = PokedexFontFamily.poppins,
+                fontWeight = FontWeight.Bold,
+                fontSize = TypographySize.s100
+            ),
+        )
+    }
+}
+
+
+sealed class PokedexFontFamily {
+    companion object {
+        val poppins = FontFamily(
+            Font(R.font.poppins_regular, FontWeight.Normal),
+            Font(R.font.poppins_bold, FontWeight.Bold)
+        )
+    }
+}
+
+sealed class TypographySize(
+    val s100: TextUnit,
+    val s200: TextUnit,
+    val s300: TextUnit,
+    val s400: TextUnit,
+    val s500: TextUnit
+) {
+    companion object :
+        TypographySize(
+            s100 = 8.sp,
+            s200 = 10.sp,
+            s300 = 12.sp,
+            s400 = 14.sp,
+            s500 = 24.sp
+        )
+}
---
+++ app/src/main/java/com/github/felipecastilhos/pokedexandroid/uicatalog/TypographyShowcase.kt
@@ -0,0 +1,88 @@
+package com.github.felipecastilhos.pokedexandroid.uicatalog
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.Center
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.github.felipecastilhos.pokedexandroid.core.ui.theme.PokedexAndroidTheme
+import com.github.felipecastilhos.pokedexandroid.core.ui.theme.PokedexTheme
+
+@Preview(name = "Regular", group = "Atoms - Typography")
+@Composable
+fun TypographyRegularShowcase() {
+    PokedexAndroidTheme {
+        Column(verticalArrangement = Arrangement.spacedBy(8.dp)) {
+            regularTypographyToArray().forEach { textStyle ->
+                Row(horizontalArrangement = Arrangement.SpaceBetween) {
+                    TypographyBox(textStyle.second)
+                    Spacer(modifier = Modifier.size(8.dp))
+                    Text(modifier = Modifier.align(Alignment.CenterVertically), text = textStyle.first)
+                }
+            }
+        }
+    }
+}
+
+@Preview(name = "Bold", group = "Atoms - Typography")
+@Composable
+fun TypographyBoldShowcase() {
+    PokedexAndroidTheme {
+        Column(verticalArrangement = Arrangement.spacedBy(8.dp)) {
+            boldTypographyToArray().forEach { textStyle ->
+                Row(horizontalArrangement = Arrangement.SpaceBetween) {
+                    TypographyBox(textStyle.second)
+                    Spacer(modifier = Modifier.size(8.dp))
+                    Text(modifier = Modifier.align(Alignment.CenterVertically), text = textStyle.first)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun TypographyBox(textStyle: TextStyle) {
+    Box(
+        modifier = Modifier
+            .size(42.dp)
+            .clip(RoundedCornerShape((10.dp)))
+            .background(PokedexTheme.colors.background.primary)
+    ) {
+        Text(
+            modifier = Modifier.align(Center),
+            text = "Aa",
+            color = PokedexTheme.colors.content.primary,
+            style = textStyle
+        )
+    }
+}
+
+@Composable
+private fun regularTypographyToArray(): List<Pair<String, TextStyle>> {
+    return mutableListOf(
+        Pair("h1", PokedexTheme.typography.regular.h1),
+        Pair("h2", PokedexTheme.typography.regular.h2),
+        Pair("body1", PokedexTheme.typography.regular.body1),
+        Pair("body2", PokedexTheme.typography.regular.body2),
+        Pair("subtitle", PokedexTheme.typography.regular.subtitle)
+    )
+}
+
+@Composable
+private fun boldTypographyToArray(): List<Pair<String, TextStyle>> {
+    return mutableListOf(
+        Pair("h1", PokedexTheme.typography.bold.h1),
+        Pair("h2", PokedexTheme.typography.bold.h2),
+        Pair("body1", PokedexTheme.typography.bold.body1),
+        Pair("body2", PokedexTheme.typography.bold.body2),
+        Pair("subtitle", PokedexTheme.typography.bold.subtitle)
+    )
+}
