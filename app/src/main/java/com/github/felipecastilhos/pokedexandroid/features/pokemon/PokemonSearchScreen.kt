8ec51868f3e6f81d41d8e3e18c1a94812420ad31
Felipe Castilhos <felipecastilhos@gmail.com>
2023-01-03 10:03:52 -0300

    fix: list layout scroll

--- app/src/main/java/com/github/felipecastilhos/pokedexandroid/features/pokemon/PokemonSearchScreen.kt
+++ app/src/main/java/com/github/felipecastilhos/pokedexandroid/features/pokemon/PokemonSearchScreen.kt
@@ -1,7 +1,7 @@
 package com.github.felipecastilhos.pokedexandroid.features.pokemon
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
@@ -9,14 +9,18 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.github.felipecastilhos.pokedexandroid.core.ui.components.HomeTopAppBar
+import com.github.felipecastilhos.pokedexandroid.core.ui.theme.PokedexTheme
 import com.github.felipecastilhos.pokedexandroid.features.pokemon.domain.viewmodel.PokemonListEntryUiData
 import com.github.felipecastilhos.pokedexandroid.features.pokemon.domain.viewmodel.PokemonListViewModel
 import com.github.felipecastilhos.pokedexandroid.features.pokemon.presentation.components.PokemonListCard
 
 @Composable
 fun PokemonSearchScreen(
+    modifier: Modifier = Modifier.background(PokedexTheme.colors.background.primary),
     viewModel: PokemonListViewModel = hiltViewModel(),
     onNavigateToPokemonDetails: () -> Unit
 ) {
@@ -26,16 +30,22 @@ fun PokemonSearchScreen(
         if (isLoading) {
             LoadingScreen()
         } else {
-            PokenonSearchList(
-                pokemons = pokemons,
-                onNavigateToPokemonDetails = onNavigateToPokemonDetails
-            )
+            Column(modifier = modifier) {
+                HomeTopAppBar(title = "Pokédex", iconPainter = painterResource(id = PokedexTheme.icons.pokeball))
+                Spacer(Modifier.height(8.dp))
+                PokenonSearchList(
+                    modifier = Modifier.fillMaxHeight(),
+                    pokemons = pokemons,
+                    onNavigateToPokemonDetails = onNavigateToPokemonDetails
+                )
+            }
         }
     }
 }
 
 @Composable
 fun PokenonSearchList(
+    modifier: Modifier = Modifier,
     pokemons: List<PokemonListEntryUiData>,
     onNavigateToPokemonDetails: () -> Unit
 ) {
